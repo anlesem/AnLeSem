9d38ba4ea9d624a8a34fac53f1546432331b2cbf
anlesem <anlesem@mail.ru>
2021-05-24 16:31:15 +0300

    js итог

--- js/scritps.js
+++ js/scritps.js
@@ -1,91 +1,93 @@
-//* Наведение в ПК версии
 
-let info_up = document.getElementById('info__up');
-let info_down = document.getElementById('info__down');
-let info_left = document.getElementById('info__left');
-let info_right = document.getElementById('info__right');
-
-
+// показ и скрытие при наведении 
 function addHover(block) {
+	removeAll();
+	viewClose();
+	mobile('add');
 	document.querySelector(block).classList.add('hover');
 }
 
-info_up.onmouseenter = addHover('.up');
-
-
-// info_up.onmouseenter = function () {
-// 	document.querySelector('.up').classList.add('hover');
-// }
-info_up.onmouseleave = function () {
-	document.querySelector('.up').classList.remove('hover');
+function removeHover(block) {
+	document.querySelector(block).classList.remove('hover');
 }
 
-info_down.onmouseenter = function () {
-	document.querySelector('.down').classList.add('hover');
-}
-info_down.onmouseleave = function () {
-	document.querySelector('.down').classList.remove('hover');
-}
 
-info_left.onmouseenter = function () {
-	document.querySelector('.left').classList.add('hover');
-}
-info_left.onmouseleave = function () {
-	document.querySelector('.left').classList.remove('hover');
-}
-
-info_right.onmouseenter = function () {
-	document.querySelector('.right').classList.add('hover');
-}
-info_right.onmouseleave = function () {
-	document.querySelector('.right').classList.remove('hover');
- }
-
-
-
-//* Закрываем открытое
-
+// Настройка отображения кнопки закрыть для мобильных устройств 
 function viewClose() {
 	cls.style.fill = '#aaa';
 	cls.style.opacity = '1';
 }
 
-let cls = document.getElementById('close');
-let tag_up = document.getElementById('up-tag');
-let tag_down = document.getElementById('down-tag');
-let tag_left = document.getElementById('left-tag');
-let tag_right = document.getElementById('right-tag');
+// Закрытие всего открытого
 
-cls.addEventListener('click', function () {
+function removeAll() {
+	mobile('remove');
 	document.querySelector('.up').classList.remove('hover');
 	document.querySelector('.down').classList.remove('hover');
 	document.querySelector('.left').classList.remove('hover');
 	document.querySelector('.right').classList.remove('hover');
 	cls.style.fill = 'rgba(0, 0, 0, .7)'
 	cls.style.opacity = '0.5'
-});
+}
+
+// дополнение для мобильных устройств (скрытие/показ наименований бирок)
+function mobile (type) {
+	if (screenView < 768) {
+		let elements = document.querySelectorAll('.title-tag');
+		for (let elem of elements) {
+			// alert(elem.innerHTML);
+			if (type == 'add')
+				elem.classList.add('hover');
+			else elem.classList.remove('hover');
+		 } 
+	}
+}
 
 
-tag_up.addEventListener('touchstart', viewClose);
-tag_down.addEventListener('touchstart', viewClose);
-tag_left.addEventListener('touchstart', viewClose);
-tag_right.addEventListener('touchstart', viewClose);
+//* ----------------------------------------------------------------
+// значение ширины экрана
+var screenView = document.documentElement.scrollWidth;
 
-tag_up.addEventListener('touchstart', function () {
-	document.querySelector('.up').classList.add('hover');
-});
+// Наведение в ПК версии
 
-tag_down.addEventListener('touchstart', function () {
-	document.querySelector('.down').classList.add('hover');
-});
+let info_up = document.getElementById('info__up');
+let info_down = document.getElementById('info__down');
+let info_left = document.getElementById('info__left');
+let info_right = document.getElementById('info__right');
 
-tag_left.addEventListener('touchstart', function () {
-	document.querySelector('.left').classList.add('hover');
-});
+info_up.addEventListener('mouseover', () => addHover('.up'));
+info_down.addEventListener('mouseover', () => addHover('.down'));
+info_left.addEventListener('mouseover', () => addHover('.left'));
+info_right.addEventListener('mouseover', () => addHover('.right'));
 
-tag_right.addEventListener('touchstart', function () {
-	document.querySelector('.right').classList.add('hover');
-});
+info_up.addEventListener('mouseout', () => removeHover('.up'));
+info_down.addEventListener('mouseout', () => removeHover('.down'));
+info_left.addEventListener('mouseout', () => removeHover('.left'));
+info_right.addEventListener('mouseout', () => removeHover('.right'));
+
+
+
+// Наведение в мобильной версии
+
+let cls = document.getElementById('close');
+let tag_up = document.getElementById('up-tag');
+let tag_down = document.getElementById('down-tag');
+let tag_left = document.getElementById('left-tag');
+let tag_right = document.getElementById('right-tag');
+let header = document.getElementById('header');
+
+tag_up.addEventListener('touchstart', () => addHover('.up'));
+tag_down.addEventListener('touchstart', () => addHover('.down'));
+tag_left.addEventListener('touchstart', () => addHover('.left'));
+tag_right.addEventListener('touchstart', () => addHover('.right'));
+
+
+// Закрытие в мобильной версии
+
+cls.addEventListener('click', removeAll);
+header.addEventListener('click', removeAll);
+
+
 
 
 
