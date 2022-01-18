23db205b4dd7f7456da3943c24fc38c9edd290b4
anlesem <anlesem@mail.ru>
2022-01-18 15:35:41 +0300

    стоп-тест: работает

--- js/scritps.js
+++ js/scritps.js
@@ -225,21 +225,20 @@ function scrollUp() {
 // Отслеживание. Движения
 function start(event) {
 	initialPoint = event.changedTouches[0];
-
 }
 
 function end(event) {
-	if (!blockActive) event.preventDefault();
+	event.preventDefault();
 	finalPoint = event.changedTouches[0];
 	var xAbs = Math.abs(initialPoint.pageX - finalPoint.pageX);
 	var yAbs = Math.abs(initialPoint.pageY - finalPoint.pageY);
 	if (xAbs > 30 || yAbs > 30) {														// Диапазон смещения. Данный весьма мал, но сбоев не обнаружено
 		if (xAbs > yAbs) {
 			if (finalPoint.pageX < initialPoint.pageX) {
-				if (blockActive && fullScreen) slide('next');			// в полноэкранном режиме переключение между input.checked/label
+				if (blockActive && fullScreen) slide('next');					// в полноэкранном режиме переключение между input.checked/label
 				else if (!blockActive) addHoverFirst('.right'); 				// Движение влево только в неактивном состоянии 
 			}
-			else if (blockActive && fullScreen) slide('prev');		// в полноэкранном режиме переключение между input.checked/label
+			else if (blockActive && fullScreen) slide('prev');					// в полноэкранном режиме переключение между input.checked/label
 			else if (!blockActive) addHoverFirst('.left');						// Движение вправо только в неактивном состоянии 
 		}
 		else if (!blockActive) {														// Только в неактивном состоянии 
